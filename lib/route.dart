bc19f41f6dc0a554f648515d7129c5b326c87a5b
UVM <utkarshmore7588@gmail.com>
2023-02-18 19:19:34 +0530

    new routes added

--- lib/route.dart
+++ lib/route.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:reddit_clone/features/auth/screens/home.dart';
+import 'package:reddit_clone/features/home/screens/home.dart';
 import 'package:reddit_clone/features/auth/screens/login.dart';
 import 'package:routemaster/routemaster.dart';
 
